cb98f3840edeb1db08677e1b69ea39bb8fa0f8a5
Stella Gonçalves <135771572+stellag2003@users.noreply.github.com>
2023-07-25 16:13:07 -0300

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes
+{
+    internal class Personagem
+    {
+        //VARIÁVEIS (atributos)
+        public string nome;
+        public string raca;
+        public int poder;
+
+        //FUNÇÔES (método)
+        public void mostra()
+        {
+            Console.WriteLine($"Seu personagem é {raca} o nome é {nome} e o poder é {poder}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Aula12Classes;
+
+Console.WriteLine("Hello, World!");
+
+Personagem guerreiro = new Personagem();
+guerreiro.nome = "Bloom";
+guerreiro.raca = "Fada";
+guerreiro.poder = 155;
+
+guerreiro.mostra();
+
+Personagem mago = new Personagem();
+mago.raca = "Mago";
+mago.poder = 200;
+mago.nome = "Harry";
+
+mago.mostra();
+
+Personagem bruxa= mago;
+bruxa.nome = "Gina";
+
+bruxa.mostra();
+
+Personagem sereia = new Personagem();
+sereia.raca = "Sereia";
+sereia.nome = "Ariel";
+sereia.poder = 23231313;
+
+sereia.mostra();
+
+Console.ReadLine();
+
